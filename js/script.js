6694320f074977a0d7e6585c03c63ea6fe74c623
Ridho Khoer <ridhokhoer123@gmail.com>
2024-11-09 18:19:21 +0700

    first commit

---
+++ js/script.js
@@ -0,0 +1,31 @@
+// Toggle class active
+const navbarNav = document.querySelector(".navbar-nav");
+
+// ketika hamburger menu di klik
+document.querySelector("#hamburger-menu").onclick = () => {
+  navbarNav.classList.toggle("active");
+};
+
+// Toogle class active untuk search form
+const searchForm = document.querySelector(".search-form");
+const searchBox = document.querySelector("#search-box");
+
+document.querySelector("#search-button").onclick = (e) => {
+  searchForm.classList.toggle("active");
+  searchBox.focus();
+  e.preventDefault(); // menghentikan event bubbling
+};
+
+// Klik di luar sidebar untuk menghilangkan nav dan tombol serach
+const hamburger = document.querySelector("#hamburger-menu");
+const searchButton = document.querySelector("#search-button");
+
+document.addEventListener("click", function (e) {
+  if (!hamburger.contains(e.target) && !navbarNav.contains(e.target)) {
+    navbarNav.classList.remove("active");
+  }
+  
+  if (!searchButton.contains(e.target) && !searchForm.contains(e.target)) {
+    searchForm.classList.remove("active");
+  }
+});
